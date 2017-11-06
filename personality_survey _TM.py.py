13f1c80f93fb3ecaff874b59c38e6c59fe72c476
tgwings8 <33422413+tgwings8@users.noreply.github.com>
2017-11-06 08:39:13 -0500

    initial commit

---
+++ personality_survey _TM.py.py
@@ -0,0 +1,65 @@
+while True:
+
+print ("What's your favorite sport?")
+sport = input().title()
+if sport == "Hockey":
+    print("Wow, hockey is my favorite sport too!")
+    print ("What's your favorite team?")
+
+    team = input()
+    if team == "NYR":
+        print("cool")
+elif sport == "Lacrosse":
+    print ("I'm great at hockey maybe I could teach you.")
+    print ("I like playing right wing.")
+else:
+    print ("I don't play" + sport)
+    
+        
+    
+print("What's your favorite TV show?")
+tvshow = input()
+
+if tvshow == "The Flash":
+    print("Who's your favorite character?")
+    character = input()
+    
+    if character == "Flash":
+        print ("Zoom,Savitar, gorilla grod")
+else:
+    print (tvshow + " sounds funny.")
+
+print ("What is your favorite videogame?")
+videogame = input ()
+if "NHL 18" in videogame
+    print ("Thats my favorite to play")
+myrestaraurants = ['Rinaldis', 'Corbos', 'Garden Catering']
+print ("Where do you like to eat?")
+food = input ()
+
+if food in myrestaraunts:
+        print("Cool")
+
+
+
+
+print ("Play again? (Yes/No)")
+answer = input ()
+if answer == "Yes":
+    print ("OK here we go!")
+else:
+    print("Buhbye")
+    break
+
+
+
+
+
+
+
+
+
+
+
+
+    
